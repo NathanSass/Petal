935094c4bf37eb3272435bc5de64f0f549914c3e
Nathan Sass <nsass711@gmail.com>
2016-02-05 10:19:09 +0900

    Implemented a basic listView of strings. Need to customize the adapter class, have the listView display event card information

--- app/src/main/java/com/nathansass/petal/LikedEventsListActivity.java
+++ app/src/main/java/com/nathansass/petal/LikedEventsListActivity.java
@@ -2,6 +2,8 @@ package com.nathansass.petal;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by nathansass on 2/4/16.
@@ -11,5 +13,13 @@ public class LikedEventsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.liked_events);
+
+        String[] myStringArray={"A","B","C"};
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, myStringArray);
+
+        ListView myList= (ListView) findViewById(R.id.likedEvents_listView);
+
+        myList.setAdapter(myAdapter);
     }
 }
