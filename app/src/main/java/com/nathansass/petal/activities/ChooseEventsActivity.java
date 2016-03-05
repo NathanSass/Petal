42966c905c0f8332f58bc2d3086244a59d377ab8
Nathan Sass <nsass711@gmail.com>
2016-03-05 16:16:08 +0630

    Displays all fields to choose events from the data model. Todo format

--- app/src/main/java/com/nathansass/petal/activities/ChooseEventsActivity.java
+++ app/src/main/java/com/nathansass/petal/activities/ChooseEventsActivity.java
@@ -2,7 +2,6 @@ package com.nathansass.petal.activities;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -16,19 +15,14 @@ import android.widget.Toast;
 import com.nathansass.petal.R;
 import com.nathansass.petal.data.ServerRequests;
 import com.nathansass.petal.data.UserLocalStore;
-import com.nathansass.petal.interfaces.GetImageCallback;
-import com.nathansass.petal.interfaces.GetImageURLSCallback;
 import com.nathansass.petal.interfaces.PutEventAttendingCallback;
 import com.nathansass.petal.models.EventCard;
 import com.nathansass.petal.models.EventDeck;
 import com.nathansass.petal.models.LikedDeck;
 import com.nathansass.petal.models.User;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Random;
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 
 public class ChooseEventsActivity extends AppCompatActivity {
 
@@ -37,12 +31,15 @@ public class ChooseEventsActivity extends AppCompatActivity {
     int duration;
     ImageView eventBanner;
 
-    EventCard mCurrentEventCard = null;
+    EventCard currentEvent = null;
     private Toolbar toolbar;
     private android.support.v7.app.ActionBar actionBar;
 
     UserLocalStore mUserLocalStore;
     User currentUser;
+
+    TextView eventTitle, eventPrice, eventAddress, eventCityState,
+            eventDate, eventAbout, eventTime, eventSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,7 +56,16 @@ public class ChooseEventsActivity extends AppCompatActivity {
         actionBar.setIcon(R.drawable.petal_logo_with_text);
 
         /* UI components */
-        eventBanner = (ImageView) findViewById(R.id.eventBanner);
+        eventBanner    = (ImageView) findViewById(R.id.eventBanner);
+        eventAddress   = (TextView) findViewById(R.id.eventAddress);
+        eventCityState = (TextView) findViewById(R.id.eventCityState);
+        eventPrice     = (TextView) findViewById(R.id.eventPrice);
+        eventTitle     = (TextView) findViewById(R.id.eventTitle);
+        eventDate      = (TextView) findViewById(R.id.eventDate);
+        eventAbout     = (TextView) findViewById(R.id.eventAbout);
+        eventTime      = (TextView) findViewById(R.id.eventTime);
+        eventSize      = (TextView) findViewById(R.id.eventSize);
+
 
         /* Get user data for the logged in user */
         mUserLocalStore = new UserLocalStore(this);
@@ -80,51 +86,60 @@ public class ChooseEventsActivity extends AppCompatActivity {
     }
 
     public void updateEventCardUI() {
-        TextView eventTitle = (TextView) findViewById(R.id.eventTitle);
 
         if ( !EventDeck.get().getDeck().isEmpty() ) {
-            mCurrentEventCard = EventDeck.get().getNewEvent();
+            currentEvent = EventDeck.get().getNewEvent();
 
-            final ServerRequests serverRequests = new ServerRequests(this);
+//            final ServerRequests serverRequests = new ServerRequests(this);
 
 
             /* param not currently being used */
-            serverRequests.fetchImageUrlsInBackground("swingdance", new GetImageURLSCallback() {
-                @Override
-                public void done(JSONArray returnedUrls) {
-                    String imageUrl = null;
-                    try {
-                        JSONObject returnedUrl = (JSONObject) returnedUrls.get(new Random().nextInt(returnedUrls.length()));
+//            serverRequests.fetchImageUrlsInBackground("swingdance", new GetImageURLSCallback() {
+//                @Override
+//                public void done(JSONArray returnedUrls) {
+//                    String imageUrl = null;
+//                    try {
+//                        JSONObject returnedUrl = (JSONObject) returnedUrls.get(new Random().nextInt(returnedUrls.length()));
+//
+//                        String farmId = returnedUrl.getInt("farm") + "";
+//                        String serverId = returnedUrl.getString("server");
+//                        String id = returnedUrl.getString("id");
+//                        String secret = returnedUrl.getString("secret");
+//                        String size = "n";
+//
+//                        imageUrl = "https://farm" + farmId + ".staticflickr.com/" + serverId + "/" + id + "_" + secret + "_" + size + ".jpg";
+//
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                    serverRequests.fetchImageInBackground(imageUrl, new GetImageCallback() {
+//                        @Override
+//                        public void done(Bitmap returnedImage) {
+//                            eventBanner.setImageBitmap(returnedImage);
+//
+//                            Toast toast = Toast.makeText(context, "Image loaded", duration);
+//                            toast.show();
+//                        }
+//                    });
+//
+//
+//                }
+//            });
 
-                        String farmId = returnedUrl.getInt("farm") + "";
-                        String serverId = returnedUrl.getString("server");
-                        String id = returnedUrl.getString("id");
-                        String secret = returnedUrl.getString("secret");
-                        String size = "n";
+            DateTime startDT = currentEvent.startDateTime;
+            DateTime endDT   = currentEvent.endDateTime;
+            LocalDate localDate   = new LocalDate(startDT);
+            String namedDayOfWeek = localDate.withDayOfWeek(2).dayOfWeek().getAsText();
 
-                        imageUrl = "https://farm" + farmId + ".staticflickr.com/" + serverId + "/" + id + "_" + secret + "_" + size + ".jpg";
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                    serverRequests.fetchImageInBackground(imageUrl, new GetImageCallback() {
-                        @Override
-                        public void done(Bitmap returnedImage) {
-                            eventBanner.setImageBitmap(returnedImage);
-
-                            Toast toast = Toast.makeText(context, "Image loaded", duration);
-                            toast.show();
-                        }
-                    });
-
-
-                }
-            });
-
-
-
-            eventTitle.setText(mCurrentEventCard.title);
+            eventAddress.setText(currentEvent.street);
+            eventCityState.setText(currentEvent.city + ", " + currentEvent.state);
+            eventPrice.setText("$" + currentEvent.price + ".00");
+            eventTitle.setText(currentEvent.title);
+            eventAbout.setText(currentEvent.about);
+            eventDate.setText( namedDayOfWeek + ", " + startDT.monthOfYear().getAsText() + " " + startDT.getDayOfMonth());
+            eventTime.setText(startDT.hourOfDay().get() + ":" + startDT.minuteOfHour().get() + " - " + endDT.hourOfDay().get() + ":" + endDT.minuteOfHour().get());
+            eventSize.setText(currentEvent.eventSize + "");
         } else {
             eventTitle.setText(R.string.deck_empty_message);
         }
@@ -135,12 +150,12 @@ public class ChooseEventsActivity extends AppCompatActivity {
 
         if ( !EventDeck.get().getDeck().isEmpty() ){
 
-            EventDeck.get().removeEvent(mCurrentEventCard);
+            EventDeck.get().removeEvent(currentEvent);
 
-            LikedDeck.get().addEvent(mCurrentEventCard);
+            LikedDeck.get().addEvent(currentEvent);
 
             ServerRequests serverRequests = new ServerRequests(this);
-            serverRequests.storeEventAttendDataInBackground(currentUser, mCurrentEventCard, true, new PutEventAttendingCallback() {
+            serverRequests.storeEventAttendDataInBackground(currentUser, currentEvent, true, new PutEventAttendingCallback() {
                 @Override
                 public void done(Void returnedRecordId) {
                     /* Nothing needs to be done */
